64530a3bcb75891e6d64b64875faf0213bf8d785
Mats Bolstad <mbolstad@gmail.com>
2025-02-26 11:08:17 +0100

    Checkbox for å oppgi om man skal søke også i lukkede (#3667)

--- src/client/app/saksbehandler/sokeboks/SøkForm.tsx
+++ src/client/app/saksbehandler/sokeboks/SøkForm.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
-import { Search } from '@navikt/ds-react';
+import { Checkbox, HStack, HelpText, Search } from '@navikt/ds-react';
+import VerticalSpacer from 'sharedComponents/VerticalSpacer';
 
-export function SøkForm(props: { loading: boolean; utførSøk: (søkeord: string) => void; nullstillSøk: () => void }) {
-	const [søkeord, setSøkeord] = useState('');
+export function SøkForm(props: {
+	loading: boolean;
+	utførSøk: (søk: { searchString: string; fraAktiv: boolean }) => void;
+	nullstillSøk: () => void;
+}) {
+	const [searchString, setSearchString] = useState('');
+	const [inkluderLukkede, setInkluderLukkede] = useState(false);
 	return (
 		<form
 			role="search"
 			onSubmit={(e) => {
 				e.preventDefault();
-				props.utførSøk(søkeord);
+				props.utførSøk({ searchString, fraAktiv: !inkluderLukkede });
 			}}
 		>
 			<Search
 				label="Søk på saksnummer, personnummer eller journalpost-id"
 				variant="primary"
 				hideLabel={false}
-				onChange={setSøkeord}
+				onChange={setSearchString}
 				htmlSize={40}
 				maxLength={11}
 				onClear={props.nullstillSøk}
@@ -23,6 +29,10 @@ export function SøkForm(props: { loading: boolean; utførSøk: (søkeord: strin
 			>
 				<Search.Button loading={props.loading}>Søk</Search.Button>
 			</Search>
+			<VerticalSpacer eightPx />
+			<Checkbox size="small" value={inkluderLukkede} onChange={() => setInkluderLukkede(!inkluderLukkede)}>
+				Søk også i lukkede saker
+			</Checkbox>
 		</form>
 	);
 }
