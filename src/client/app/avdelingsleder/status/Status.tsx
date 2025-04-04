3668012d21f8c7756099a7df80f31b06143d1528
Mats Bolstad <mbolstad@gmail.com>
2025-04-04 13:52:00 +0200

    Bindestrek på rett sted

--- src/client/app/avdelingsleder/status/Status.tsx
+++ src/client/app/avdelingsleder/status/Status.tsx
@@ -4,6 +4,16 @@ import { useHentAvdelingslederStatus } from 'api/queries/avdelingslederQueries';
 import EnkelTeller from './EnkelTeller';
 import * as styles from './Status.css';
 
+// Pynter visningsnavnet som kommer fra backend
+function visningsnavn(behandlingstype: string) {
+	switch (behandlingstype) {
+		case 'Førstegangsbehandling':
+			return <>Førstegangs&shy;behandling</>; // Legger på bindestrek på riktig sted hvis nødvendig
+		default:
+			return behandlingstype;
+	}
+}
+
 export default function Status() {
 	const { data, isSuccess } = useHentAvdelingslederStatus();
 	return (
@@ -12,7 +22,7 @@ export default function Status() {
 			{isSuccess && (
 				<HStack>
 					{data.map(({ antall, behandlingstype }) => (
-						<EnkelTeller antall={antall} tekst={behandlingstype} />
+						<EnkelTeller antall={antall} tekst={visningsnavn(behandlingstype)} />
 					))}
 				</HStack>
 			)}
