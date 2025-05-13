7178cb7d0460e5b6db8091eba3297e7f49f5acef
Mats Bolstad <mbolstad@gmail.com>
2025-05-13 13:23:34 +0200

    Håndtering av loading og error på siste-oppgaver-kall

--- src/client/app/saksbehandler/saksstotte/components/SisteOppgaver.tsx
+++ src/client/app/saksbehandler/saksstotte/components/SisteOppgaver.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { Heading } from '@navikt/ds-react';
+import { Heading, Skeleton } from '@navikt/ds-react';
 import { useHentSisteOppgaver } from 'api/queries/saksbehandlerQueries';
+import Repeat from 'sharedComponents/Repeat';
 
 export default function SisteOppgaver() {
-	const { data, isSuccess } = useHentSisteOppgaver();
-	if (!isSuccess || data === undefined || data.length === 0) return null;
+	const { data, isSuccess, isLoading, isError } = useHentSisteOppgaver();
 
 	return (
 		<div>
 			<Heading size="xsmall">Dine siste oppgaver</Heading>
+			{isSuccess && data.length === 0 && 'Ingen oppgaver'}
+			{isError && 'Feil ved henting av oppgaver'}
 			<ul className="mt-2 list-none pl-0">
-				{data.map((oppgave) => (
-					<li key={oppgave.oppgaveEksternId}>
-						{oppgave.url ? <a href={oppgave.url}>{oppgave.tittel}</a> : oppgave.tittel}
-					</li>
-				))}
+				{isLoading && (
+					<Repeat times={10}>
+						<li>
+							<Skeleton width={250} />
+						</li>
+					</Repeat>
+				)}
+				{isSuccess &&
+					data.length > 0 &&
+					data.map((oppgave) => (
+						<li key={oppgave.oppgaveEksternId}>
+							{oppgave.url ? <a href={oppgave.url}>{oppgave.tittel}</a> : oppgave.tittel}
+						</li>
+					))}
 			</ul>
 		</div>
 	);
