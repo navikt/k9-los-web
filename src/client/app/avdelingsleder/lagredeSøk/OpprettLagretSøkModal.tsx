a95a8beeef39cbbfb28827c2d9598b9a194f3a4e
Mats Bolstad <mbolstad@gmail.com>
2025-07-22 14:43:10 +0200

    Opprett søk

--- src/client/app/avdelingsleder/lagredeSøk/OpprettLagretSøkModal.tsx
+++ src/client/app/avdelingsleder/lagredeSøk/OpprettLagretSøkModal.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import { Modal } from '@navikt/ds-react';
+import { useForm } from 'react-hook-form';
+import { Button, Heading, Modal } from '@navikt/ds-react';
+import { useOpprettLagretSøk } from 'api/queries/avdelingslederQueries';
 import { RenderModalProps } from 'sharedComponents/ModalButton';
 
 export function OpprettLagretSøkModal({ open, closeModal }: RenderModalProps) {
+	const { mutate: opprettLagretSøk } = useOpprettLagretSøk();
 	return (
 		<Modal open={open} onClose={closeModal} aria-label="Opprett lagret søk" closeOnBackdropClick>
-			<span>Opprett nytt lagret søk</span>
+			<form></form>
+			<Modal.Body>
+				<Heading size="medium">Nytt lagret søk</Heading>
+			</Modal.Body>
+			<Modal.Footer>
+				<Button></Button>
+			</Modal.Footer>
 		</Modal>
 	);
 }
