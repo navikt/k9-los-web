e774671007cf2dc9631e894844155a7c773d0b8a
Mats Bolstad <mbolstad@gmail.com>
2025-07-23 17:26:31 +0200

    Endringsmodal

--- src/client/app/avdelingsleder/lagredeSøk/LagredeSøkTabell.tsx
+++ src/client/app/avdelingsleder/lagredeSøk/LagredeSøkTabell.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Skeleton, Table } from '@navikt/ds-react';
+import React, { useState } from 'react';
+import { Button, Modal, Skeleton, SortState, Table } from '@navikt/ds-react';
 import { LagretSøk, useHentAntallLagretSøk } from 'api/queries/avdelingslederQueries';
+import { EndreLagretSøkModal } from 'avdelingsleder/lagredeSøk/EndreLagretSøkModal';
+import ModalButton from 'sharedComponents/ModalButton';
 import { momentDateFormat } from 'utils/dateUtils';
 
 function AntallLagretSøk({ id }: { id: number }) {
@@ -15,23 +17,51 @@ function AntallLagretSøk({ id }: { id: number }) {
 }
 
 export function LagredeSøkTabell(props: { lagredeSøk: LagretSøk[] }) {
+	const [sort, setSort] = useState<SortState | undefined>({
+		orderBy: 'tittel',
+		direction: 'ascending',
+	});
+
+	const handleSort = (sortKey: string) => {
+		const newDirection =
+			sort && sortKey === sort.orderBy && sort.direction === 'ascending' ? 'descending' : 'ascending';
+		setSort((prevState) =>
+			prevState && sortKey === prevState.orderBy && prevState.direction === 'descending'
+				? undefined
+				: { orderBy: sortKey, direction: newDirection },
+		);
+	};
+
 	return (
-		<Table>
+		<Table sort={sort} onSortChange={handleSort}>
 			<Table.Header>
 				<Table.Row>
-					<Table.HeaderCell scope="col">Tittel</Table.HeaderCell>
-					<Table.HeaderCell scope="col">Antall oppgaver</Table.HeaderCell>
-					<Table.HeaderCell scope="col">Sist endret</Table.HeaderCell>
+					<Table.ColumnHeader sortable sortKey="tittel" scope="col">
+						Tittel
+					</Table.ColumnHeader>
+					<Table.ColumnHeader scope="col">Antall oppgaver</Table.ColumnHeader>
+					<Table.ColumnHeader sortable sortKey="sistEndret" scope="col">
+						Sist endret
+					</Table.ColumnHeader>
+					<Table.HeaderCell scope="col" />
 				</Table.Row>
 			</Table.Header>
 			<Table.Body>
-				{props.lagredeSøk.map(({ id, tittel, sistEndret }) => (
-					<Table.Row key={id}>
-						<Table.DataCell>{tittel}</Table.DataCell>
+				{props.lagredeSøk.map((lagretSøk) => (
+					<Table.Row key={lagretSøk.id}>
+						<Table.DataCell>{lagretSøk.tittel}</Table.DataCell>
 						<Table.DataCell>
-							<AntallLagretSøk id={id} />
+							<AntallLagretSøk id={lagretSøk.id} />
 						</Table.DataCell>
-						<Table.DataCell>{momentDateFormat(sistEndret)}</Table.DataCell>
+						<Table.DataCell>{momentDateFormat(lagretSøk.sistEndret)}</Table.DataCell>
+						<Table.DataCell>
+							<ModalButton
+								renderButton={({ openModal }) => <Button onClick={openModal}>Endre</Button>}
+								renderModal={({ open, closeModal }) => (
+									<EndreLagretSøkModal lagretSøk={lagretSøk} open={open} closeModal={closeModal} />
+								)}
+							/>
+						</Table.DataCell>
 					</Table.Row>
 				))}
 			</Table.Body>
