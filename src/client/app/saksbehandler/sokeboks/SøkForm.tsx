9d48c3703ba39ba8db4dbbb6962acdc4bfcfbbb2
Mats Bolstad <mbolstad@gmail.com>
2025-09-16 11:07:44 +0200

    Fjerner whitespace fra søkeordet

--- src/client/app/saksbehandler/sokeboks/SøkForm.tsx
+++ src/client/app/saksbehandler/sokeboks/SøkForm.tsx
@@ -7,6 +7,12 @@ export function SøkForm(props: {
 	nullstillSøk: () => void;
 }) {
 	const [søkeord, setSøkeord] = useState('');
+
+	const endreSøkeord = (ord: string) => {
+		// Fjerner all whitespace fra søkeordet
+		setSøkeord(ord.replace(/\s/g, ''));
+	};
+
 	return (
 		<form
 			role="search"
@@ -22,10 +28,11 @@ export function SøkForm(props: {
 				label="Søk på saksnummer, personnummer eller journalpost-id"
 				variant="primary"
 				hideLabel={false}
-				onChange={setSøkeord}
+				onChange={endreSøkeord}
 				htmlSize={40}
 				maxLength={11}
 				onClear={props.nullstillSøk}
+				value={søkeord}
 				clearButton
 			>
 				<Search.Button loading={props.loading}>Søk</Search.Button>
