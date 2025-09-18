1202bba01400e08013ea2f740cca0611add75c31
Mats Bolstad <mbolstad@gmail.com>
2025-09-18 14:31:36 +0200

    Kuttet v1-funksjonalitet, ser ut til å være kompatibel både med ny og gammel backend

--- src/client/app/app/envVariablesUtils.tsx
+++ src/client/app/app/envVariablesUtils.tsx
@@ -2,9 +2,7 @@ declare global {
 	interface Window {
 		env: {
 			OIDC_PROXY_URL?: string;
-			SOKEBOKS_NYE_KOER?: string;
-			SAKSBEHANDLER_KAN_VELGE_NYE_KOER?: string;
-			AVDELINGSLEDER_TILGANG_TIL_NYE_KOER?: string;
+			EXAMPLE?: string;
 		};
 	}
 }
@@ -15,9 +13,7 @@ interface EnvVariable {
 }
 
 interface EnvVariables {
-	SOKEBOKS_NYE_KOER: string;
-	SAKSBEHANDLER_KAN_VELGE_NYE_KOER: string;
-	AVDELINGSLEDER_TILGANG_TIL_NYE_KOER: string;
+	EXAMPLE: string;
 }
 
 export const setEnvVariables = async () => {
@@ -26,9 +22,7 @@ export const setEnvVariables = async () => {
 		const data: EnvVariable[] = await response.json();
 		const envVariables = data.reduce((acc, { key, value }) => ({ ...acc, [key]: value }), {}) as EnvVariables;
 		window.env = window?.env || {};
-		window.env.SOKEBOKS_NYE_KOER = envVariables.SOKEBOKS_NYE_KOER;
-		window.env.SAKSBEHANDLER_KAN_VELGE_NYE_KOER = envVariables.SAKSBEHANDLER_KAN_VELGE_NYE_KOER;
-		window.env.AVDELINGSLEDER_TILGANG_TIL_NYE_KOER = envVariables.AVDELINGSLEDER_TILGANG_TIL_NYE_KOER;
+		window.env.EXAMPLE = envVariables.EXAMPLE;
 	} catch (error) {
 		console.error('Failed to set environment variables', error);
 		throw error;
@@ -36,7 +30,3 @@ export const setEnvVariables = async () => {
 
 	return Promise.resolve();
 };
-
-export const sokeboksNyeKoer = () => window?.env?.SOKEBOKS_NYE_KOER === 'enabled';
-export const saksbehandlerKanVelgeNyeKoer = () => window?.env?.SAKSBEHANDLER_KAN_VELGE_NYE_KOER === 'enabled';
-export const avdelingslederTilgangTilNyeKoer = () => window?.env?.AVDELINGSLEDER_TILGANG_TIL_NYE_KOER === 'enabled';
