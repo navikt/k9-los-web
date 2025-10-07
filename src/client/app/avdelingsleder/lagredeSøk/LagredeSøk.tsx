b38413f9d94189abb15f2a8327d8bd8e27c6aabf
Mats Bolstad <mbolstad@gmail.com>
2025-10-07 13:55:46 +0200

    Forbedringer på visning dersom man ikke er i saksbehandler-tabell

--- src/client/app/avdelingsleder/lagredeSøk/LagredeSøk.tsx
+++ src/client/app/avdelingsleder/lagredeSøk/LagredeSøk.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import { PlusCircleIcon } from '@navikt/aksel-icons';
-import { Button } from '@navikt/ds-react';
+import { Alert, Button } from '@navikt/ds-react';
 import { useHentLagredeSøk } from 'api/queries/avdelingslederQueries';
 import { LagredeSøkTabell } from 'avdelingsleder/lagredeSøk/LagredeSøkTabell';
 import { OpprettLagretSøkModal } from 'avdelingsleder/lagredeSøk/OpprettLagretSøkModal';
 import ModalButton from 'sharedComponents/ModalButton';
 
 export function LagredeSøk() {
-	const { data, isSuccess } = useHentLagredeSøk();
+	const { data, isSuccess, isError } = useHentLagredeSøk({ retry: false });
 	return (
 		<>
 			<ModalButton
 				renderButton={({ openModal }) => (
-					<Button className="mb-7" variant="primary" onClick={openModal} icon={<PlusCircleIcon />}>
+					<Button className="mb-7" variant="primary" onClick={openModal} icon={<PlusCircleIcon />} disabled={isError}>
 						Legg til nytt lagret søk
 					</Button>
 				)}
 				renderModal={({ open, closeModal }) => <OpprettLagretSøkModal open={open} closeModal={closeModal} />}
 			/>
+			{isError && (
+				<div>
+					<Alert variant="warning">
+						Innlogget bruker er ikke i saksbehandler-tabellen. For å opprette lagrede søk må du være lagt til som
+						saksbehandler.
+					</Alert>
+				</div>
+			)}
 			{isSuccess && data.length > 0 && <LagredeSøkTabell lagredeSøk={data} />}
 			{isSuccess && data.length === 0 && (
 				<div>
