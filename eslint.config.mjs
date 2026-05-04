ec5fcade519645f042697957f771474d2cf58142
Mats Bolstad <mbolstad@gmail.com>
2026-05-04 10:08:45 +0200

    Moderniser eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,104 +1,115 @@
-import { fixupPluginRules } from '@eslint/compat';
-import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import jestPlugin from 'eslint-plugin-jest';
+import jestDom from 'eslint-plugin-jest-dom';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
+import storybook from 'eslint-plugin-storybook';
 import globals from 'globals';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import tseslint from 'typescript-eslint';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-	recommendedConfig: js.configs.recommended,
-	allConfig: js.configs.all,
-});
-
-export default [
-	...compat.extends('airbnb', 'plugin:@typescript-eslint/recommended', 'prettier'),
+export default tseslint.config(
 	{
-		plugins: {
-			'@typescript-eslint': typescriptEslint,
-			'react-hooks': fixupPluginRules(reactHooks),
-		},
-
+		ignores: [
+			'dist/',
+			'build/',
+			'node_modules/',
+			'coverage/',
+			'.yarn/',
+			'storybook-static/',
+			'**/*.d.ts',
+		],
+	},
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	reactPlugin.configs.flat.recommended,
+	reactPlugin.configs.flat['jsx-runtime'],
+	jsxA11y.flatConfigs.recommended,
+	importPlugin.flatConfigs.recommended,
+	importPlugin.flatConfigs.typescript,
+	...storybook.configs['flat/recommended'],
+	prettier,
+	{
 		languageOptions: {
 			globals: {
 				...globals.browser,
 			},
-
-			parser: tsParser,
-			ecmaVersion: 5,
-			sourceType: 'module',
-
 			parserOptions: {
 				ecmaFeatures: {
-					ecmaVersion: 8,
 					jsx: true,
-					impliedStrict: true,
 				},
 			},
 		},
-
 		settings: {
+			react: { version: 'detect' },
 			'import/resolver': {
 				typescript: {},
-
 				node: {
 					extensions: ['.js', '.jsx', '.ts', '.tsx', '.less', '.scss'],
 					moduleDirectory: ['node_modules', 'src/'],
 				},
 			},
 		},
-
+		plugins: {
+			'react-hooks': reactHooks,
+		},
 		rules: {
-			'import/no-extraneous-dependencies': 0,
+			...reactHooks.configs.recommended.rules,
 
-			'import/no-unused-modules': [
-				1,
-				{
-					unusedExports: true,
-					missingExports: true,
-					ignoreExports: [],
-				},
-			],
+			// react-hooks v7 introduserte flere nye regler som krever omfattende
+			// refactoring av eksisterende komponenter. Demp til 'warn' inntil
+			// videre slik at de er synlige uten å blokkere.
+			'react-hooks/set-state-in-effect': 'warn',
+			'react-hooks/refs': 'warn',
+			'react-hooks/use-memo': 'warn',
+			'react-hooks/incompatible-library': 'warn',
+			'react-hooks/preserve-manual-memoization': 'warn',
+			'react-hooks/purity': 'warn',
+			'react-hooks/static-components': 'warn',
+			'react-hooks/unsupported-syntax': 'warn',
+			'react-hooks/component-hook-factories': 'warn',
+			'react-hooks/error-boundaries': 'warn',
+			'react-hooks/gating': 'warn',
+			'react-hooks/globals': 'warn',
+			'react-hooks/immutability': 'warn',
 
+			'import/no-extraneous-dependencies': 'off',
 			'import/no-unresolved': 'error',
+			'import/prefer-default-export': 'off',
+			'import/extensions': 'off',
+
 			'@typescript-eslint/no-unused-vars': 'warn',
 			'@typescript-eslint/no-use-before-define': 'error',
 			'@typescript-eslint/no-shadow': 'error',
+			'no-shadow': 'off',
+
+			'max-len': ['warn', 160],
 
 			'react/jsx-filename-extension': [
-				1,
-				{
-					extensions: ['.js', '.jsx', '.ts', '.tsx'],
-				},
+				'warn',
+				{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
 			],
-
-			'no-shadow': 'off',
-			'max-len': [1, 160],
 			'react/jsx-props-no-spreading': 'off',
 			'react/destructuring-assignment': 'off',
-			'jsx-a11y/control-has-associated-label': 'off',
 			'react/function-component-definition': 'off',
-			'implicit-arrow-linebreak': 'off',
-			'function-paren-newline': 'off',
-			'arrow-parens': 'off',
-			'object-curly-newline': 'off',
 			'react/require-default-props': 'off',
 			'react/prop-types': 'off',
-			'import/prefer-default-export': 'off',
-			'import/extensions': 'off',
+
+			'jsx-a11y/control-has-associated-label': 'off',
 		},
 	},
 	{
 		files: ['**/*.spec.ts', '**/*.spec.tsx'],
-
+		plugins: {
+			jest: jestPlugin,
+			'jest-dom': jestDom,
+		},
 		rules: {
-			'@typescript-eslint/no-explicit-any': 0,
-			'import/no-unused-modules': 0,
+			...jestPlugin.configs.recommended.rules,
+			...jestDom.configs.recommended.rules,
+			'@typescript-eslint/no-explicit-any': 'off',
 		},
 	},
-];
+);
