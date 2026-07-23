fc414f1944a6c499e6b4034a0875b166637fbc04
Mats Bolstad <mbolstad@gmail.com>
2026-07-23 16:29:20 +0200

    Oppgraderer til tailwind v4

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,5 @@
-const autoprefixer = require('autoprefixer');
-const postcssImport = require('postcss-import');
-const tailwindcss = require('tailwindcss');
-
 module.exports = {
-	plugins: [postcssImport, tailwindcss('./tailwind.config.cjs'), autoprefixer],
+	plugins: {
+		'@tailwindcss/postcss': {},
+	},
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,4 @@ const dsTailwind = require('@navikt/ds-tailwind');
 module.exports = {
 	presets: [dsTailwind],
 	content: ['./src/**/*.{js,jsx,ts,tsx}', '/dist/index.html'],
-	corePlugins: {
-		preflight: false,
-	},
-	plugins: [],
 };
