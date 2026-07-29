b1be0557c68160ed4cfa18454c431d43ffbf055c
Mats Bolstad <mbolstad@gmail.com>
2026-07-29 15:35:30 +0200

    Bruker ikke context på behandlingskoer lenger, så slipper memoisering

--- src/client/app/saksbehandler/behandlingskoer/BehandlingskoerIndex.tsx
+++ src/client/app/saksbehandler/behandlingskoer/BehandlingskoerIndex.tsx
@@ -1,12 +1,10 @@
 import { Loader } from '@navikt/ds-react';
 import { useAlleSaksbehandlerKoerV3 } from 'api/queries/saksbehandlerQueries';
-import { type FunctionComponent, useState } from 'react';
-import BehandlingskoerContext from 'saksbehandler/BehandlingskoerContext';
+import type { FunctionComponent } from 'react';
 import type { OppgavekøV3, OppgavekøV3MedNavn } from 'types/OppgavekøV3Type';
 import OppgavekoPanel from './components/OppgavekoPanel';
 
 const BehandlingskoerIndex: FunctionComponent = () => {
-	const [valgtOppgavekoId, setValgtOppgavekoId] = useState('');
 	const { data: oppgavekoerV3, isLoading } = useAlleSaksbehandlerKoerV3();
 
 	const mapKøV3 = (kø: OppgavekøV3): OppgavekøV3MedNavn => ({ ...kø, navn: kø.tittel, id: `${kø.id}__V3` });
@@ -16,17 +14,7 @@ const BehandlingskoerIndex: FunctionComponent = () => {
 		return <Loader />;
 	}
 
-	return (
-		<BehandlingskoerContext.Provider
-			value={{
-				oppgavekoer,
-				setValgtOppgavekoId,
-				valgtOppgavekoId,
-			}}
-		>
-			<OppgavekoPanel />
-		</BehandlingskoerContext.Provider>
-	);
+	return <OppgavekoPanel oppgavekoer={oppgavekoer} />;
 };
 
 export default BehandlingskoerIndex;
