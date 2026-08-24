1d7901e64d922b0cbe8ce27510de517084c547b9
Mats Bolstad <mbolstad@gmail.com>
2026-08-24 17:06:16 +0200

    Oppdatering av pakker (#4366)

--- src/client/app/api/queries/saksbehandlerQueries.ts
+++ src/client/app/api/queries/saksbehandlerQueries.ts
@@ -69,17 +69,17 @@ export const useReserverOppgaveMutation = () => {
 	});
 };
 
-export const useEndreReservasjoner = (onSuccces?: () => void) => {
+export const useEndreReservasjoner = (onSuccess?: () => void) => {
 	const queryClient = useQueryClient();
 	return useMutation<null, Error, EndreOppgaveType[]>({
-		mutationFn: (data) => axiosInstance.post(apiPaths.endreReservasjoner, data),
+		mutationFn: (data) => axiosInstance.post<null>(apiPaths.endreReservasjoner, data).then((response) => response.data),
 		onSuccess: async () => {
 			queryClient.removeQueries({ queryKey: [apiPaths.hentAktivReservasjonForOppgave] });
 			await Promise.all([
 				queryClient.invalidateQueries({ queryKey: [apiPaths.saksbehandlerReservasjoner] }),
 				queryClient.invalidateQueries({ queryKey: [apiPaths.avdelinglederReservasjoner] }),
 			]);
-			if (onSuccces) onSuccces();
+			if (onSuccess) onSuccess();
 		},
 	});
 };
